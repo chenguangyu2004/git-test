9ec70d44621722ebce4f68667e26b6af49ac4c88
chenguangyu <3069607974@qq.com>
2025-02-08 23:03:39 +0800

    master commit4

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -2,6 +2,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("got-fix 4");
-        System.out.println("master test");
+        System.out.println("push test");
     }
 }
